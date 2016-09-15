e63c52bb38c76d99c1a50da467451899b0dd0a4c
rodrigo.perez <rodrigo@frozenlogicstudios.com>
2016-09-14 23:38:18 -0300

    Added filter validation

--- frontend/js/components/filter-form.js
+++ frontend/js/components/filter-form.js
@@ -6,11 +6,21 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
 var _reduxForm = require('redux-form');
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -19,6 +29,17 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var renderField = function renderField(_ref) {
+    var input = _ref.input;
+    var placeholder = _ref.placeholder;
+    var min = _ref.min;
+    var type = _ref.type;
+    var _ref$meta = _ref.meta;
+    var touched = _ref$meta.touched;
+    var error = _ref$meta.error;
+    return _react2.default.createElement('input', _extends({}, input, { placeholder: placeholder, min: min, type: type, className: (0, _classnames2.default)({ error: touched && error }) }));
+};
 
 var FilterForm = function (_Component) {
     _inherits(FilterForm, _Component);
@@ -32,7 +53,9 @@ var FilterForm = function (_Component) {
     _createClass(FilterForm, [{
         key: 'render',
         value: function render() {
-            var handleSubmit = this.props.handleSubmit;
+            var _props = this.props;
+            var handleSubmit = _props.handleSubmit;
+            var submitting = _props.submitting;
 
 
             return _react2.default.createElement(
@@ -49,7 +72,7 @@ var FilterForm = function (_Component) {
                             { htmlFor: 'id' },
                             'Id'
                         ),
-                        _react2.default.createElement(_reduxForm.Field, { id: 'id', name: 'id', component: 'input', type: 'number' })
+                        _react2.default.createElement(_reduxForm.Field, { id: 'id', name: 'id', component: renderField, type: 'number', min: '1' })
                     ),
                     _react2.default.createElement(
                         'div',
@@ -59,7 +82,7 @@ var FilterForm = function (_Component) {
                             { htmlFor: 'area' },
                             'Área'
                         ),
-                        _react2.default.createElement(_reduxForm.Field, { id: 'area', name: 'area', component: 'input', type: 'number' })
+                        _react2.default.createElement(_reduxForm.Field, { id: 'area', name: 'area', component: renderField, type: 'number', min: '1' })
                     )
                 ),
                 _react2.default.createElement(
@@ -73,7 +96,7 @@ var FilterForm = function (_Component) {
                             { htmlFor: 'beds' },
                             'Quartos'
                         ),
-                        _react2.default.createElement(_reduxForm.Field, { id: 'beds', name: 'quartos', component: 'input', type: 'number' })
+                        _react2.default.createElement(_reduxForm.Field, { id: 'beds', name: 'quartos', component: renderField, type: 'number', min: '1' })
                     ),
                     _react2.default.createElement(
                         'div',
@@ -83,7 +106,7 @@ var FilterForm = function (_Component) {
                             { htmlFor: 'baths' },
                             'Banheiros'
                         ),
-                        _react2.default.createElement(_reduxForm.Field, { id: 'baths', name: 'banheiros', component: 'input', type: 'number' })
+                        _react2.default.createElement(_reduxForm.Field, { id: 'baths', name: 'banheiros', component: renderField, type: 'number', min: '1' })
                     )
                 ),
                 _react2.default.createElement(
@@ -100,16 +123,16 @@ var FilterForm = function (_Component) {
                         _react2.default.createElement(
                             'div',
                             { className: 'center-align' },
-                            _react2.default.createElement(_reduxForm.Field, { id: 'price', name: 'precoMin', component: 'input', type: 'text', placeholder: 'Mínimo' }),
+                            _react2.default.createElement(_reduxForm.Field, { id: 'price', name: 'precoMin', component: renderField, type: 'number', placeholder: 'Mínimo', min: '1' }),
                             _react2.default.createElement('div', { className: 'separator' }),
-                            _react2.default.createElement(_reduxForm.Field, { name: 'precoMax', component: 'input', type: 'text', placeholder: 'Máximo' })
+                            _react2.default.createElement(_reduxForm.Field, { name: 'precoMax', component: renderField, type: 'number', placeholder: 'Máximo', min: '1' })
                         )
                     )
                 ),
                 _react2.default.createElement(
                     'div',
                     { className: 'submit-row' },
-                    _react2.default.createElement('input', { type: 'submit', value: 'Filtrar' })
+                    _react2.default.createElement('input', { type: 'submit', value: 'Filtrar', disabled: submitting })
                 )
             );
         }
@@ -119,10 +142,65 @@ var FilterForm = function (_Component) {
 }(_react.Component);
 
 FilterForm.propTypes = {
-    handleSubmit: _react.PropTypes.func.isRequired
+    handleSubmit: _react.PropTypes.func.isRequired,
+    submitting: _react.PropTypes.bool.isRequired
+};
+
+var validate = function validate(values) {
+    var errors = {};
+
+    if (typeof values.id !== 'undefined' && values.id !== '') {
+        var id = parseInt(values.id);
+        if (_lodash2.default.isNaN(id) || id !== parseFloat(values.id) || id < 1) {
+            errors.id = 'Not valid id';
+        }
+    }
+
+    if (typeof values.area !== 'undefined' && values.area !== '') {
+        var area = parseInt(values.area);
+        if (_lodash2.default.isNaN(area) || area !== parseFloat(values.area) || area < 1) {
+            errors.area = 'Not valid area';
+        }
+    }
+
+    if (typeof values.quartos !== 'undefined' && values.quartos !== '') {
+        var quartos = parseInt(values.quartos);
+        if (_lodash2.default.isNaN(quartos) || quartos !== parseFloat(values.quartos) || quartos < 1) {
+            errors.quartos = 'Not valid quartos';
+        }
+    }
+
+    if (typeof values.banheiros !== 'undefined' && values.banheiros !== '') {
+        var banheiros = parseInt(values.banheiros);
+        if (_lodash2.default.isNaN(banheiros) || banheiros !== parseFloat(values.banheiros) || banheiros < 1) {
+            errors.banheiros = 'Not valid banheiros';
+        }
+    }
+
+    if (typeof values.precoMin !== 'undefined' && values.precoMin !== '') {
+        var precoMin = parseInt(values.precoMin);
+        if (_lodash2.default.isNaN(precoMin) || precoMin !== parseFloat(values.precoMin) || precoMin < 1) {
+            errors.precoMin = 'Not valid precoMin';
+        }
+    }
+
+    if (typeof values.precoMax !== 'undefined' && values.precoMax !== '') {
+        var precoMax = parseInt(values.precoMax);
+        if (_lodash2.default.isNaN(precoMax) || precoMax !== parseFloat(values.precoMax) || precoMax < 1) {
+            errors.precoMax = 'Not valid precoMax';
+        }
+    }
+
+    if (typeof values.precoMin !== 'undefined' && values.precoMin !== '' && typeof values.precoMax !== 'undefined' && values.precoMax !== '' && parseInt(values.precoMin) > parseInt(values.precoMax)) {
+        errors.precoMin = 'Not valid precoMin';
+        errors.precoMax = 'Not valid precoMax';
+    }
+
+    return errors;
 };
 
 exports.default = (0, _reduxForm.reduxForm)({
     form: 'filter',
-    enableReinitialize: true
+    enableReinitialize: true,
+    validate: validate
 })(FilterForm);
--- frontend/src/js/components/filter-form.js
+++ frontend/src/js/components/filter-form.js
@@ -2,6 +2,13 @@
 
 import React, {Component, PropTypes} from 'react';
 import { Field, reduxForm } from 'redux-form';
+import _ from 'lodash';
+import classNames from 'classnames';
+
+const renderField = ({ input, placeholder, min, type, meta: { touched, error } }) => (
+    <input {...input} placeholder={placeholder} min={min} type={type} className={classNames({ error: touched && error }) }/>
+);
+
 
 class FilterForm extends Component {
     constructor(props) {
@@ -10,42 +17,42 @@ class FilterForm extends Component {
 
     render() {
 
-        const { handleSubmit } = this.props;
+        const { handleSubmit, submitting } = this.props;
 
         return (
             <form className="form" onSubmit={handleSubmit}>
                 <div className="row">
                     <div>
                         <label htmlFor="id">Id</label>
-                        <Field id="id" name="id" component="input" type="number" />
+                        <Field id="id" name="id" component={renderField} type="number" min="1"/>
                     </div>
                     <div>
                         <label htmlFor="area">Área</label>
-                        <Field id="area" name="area" component="input" type="number" />
+                        <Field id="area" name="area" component={renderField} type="number" min="1"/>
                     </div>
                 </div>
                 <div className="row">
                     <div>
                         <label htmlFor="beds">Quartos</label>
-                        <Field id="beds" name="quartos" component="input" type="number" />
+                        <Field id="beds" name="quartos" component={renderField} type="number" min="1" />
                     </div>
                     <div>
                         <label htmlFor="baths">Banheiros</label>
-                        <Field id="baths" name="banheiros" component="input" type="number" />
+                        <Field id="baths" name="banheiros" component={renderField} type="number" min="1" />
                     </div>
                 </div>
                 <div className="row">
                     <div>
                         <label htmlFor="price">Valor</label>
                         <div className="center-align">
-                            <Field id="price" name="precoMin" component="input" type="text" placeholder="Mínimo" />
+                            <Field id="price" name="precoMin" component={renderField} type="number" placeholder="Mínimo" min="1" />
                             <div className="separator"></div>
-                            <Field name="precoMax" component="input" type="text" placeholder="Máximo" />
+                            <Field name="precoMax" component={renderField} type="number" placeholder="Máximo" min="1"/>
                         </div>
                     </div>
                 </div>
                 <div className="submit-row">
-                    <input type="submit" value="Filtrar" />
+                    <input type="submit" value="Filtrar" disabled={submitting} />
                 </div>
 
             </form>
@@ -55,10 +62,67 @@ class FilterForm extends Component {
 }
 
 FilterForm.propTypes = {
-    handleSubmit: PropTypes.func.isRequired
+    handleSubmit: PropTypes.func.isRequired,
+    submitting: PropTypes.bool.isRequired
+};
+
+
+
+const validate = values => {
+    const errors = {};
+
+    if (typeof values.id !== 'undefined' && values.id !== '') {
+        const id = parseInt(values.id);
+        if(_.isNaN(id) || id !== parseFloat(values.id) || id < 1) {
+            errors.id = 'Not valid id';
+        }
+    }
+
+    if (typeof values.area !== 'undefined' && values.area !== '') {
+        const area = parseInt(values.area);
+        if(_.isNaN(area) || area !== parseFloat(values.area) || area < 1) {
+            errors.area = 'Not valid area';
+        }
+    }
+
+    if (typeof values.quartos !== 'undefined' && values.quartos !== '') {
+        const quartos = parseInt(values.quartos);
+        if(_.isNaN(quartos) || quartos !== parseFloat(values.quartos) || quartos < 1) {
+            errors.quartos = 'Not valid quartos';
+        }
+    }
+
+    if (typeof values.banheiros !== 'undefined' && values.banheiros !== '') {
+        const banheiros = parseInt(values.banheiros);
+        if(_.isNaN(banheiros) || banheiros !== parseFloat(values.banheiros) || banheiros < 1) {
+            errors.banheiros = 'Not valid banheiros';
+        }
+    }
+
+    if (typeof values.precoMin !== 'undefined' && values.precoMin !== '') {
+        const precoMin = parseInt(values.precoMin);
+        if(_.isNaN(precoMin) || precoMin !== parseFloat(values.precoMin) || precoMin < 1) {
+            errors.precoMin = 'Not valid precoMin';
+        }
+    }
+
+    if (typeof values.precoMax !== 'undefined' && values.precoMax !== '') {
+        const precoMax = parseInt(values.precoMax);
+        if(_.isNaN(precoMax) || precoMax !== parseFloat(values.precoMax) || precoMax < 1) {
+            errors.precoMax = 'Not valid precoMax';
+        }
+    }
+
+    if(typeof values.precoMin !== 'undefined' && values.precoMin !== '' && typeof values.precoMax !== 'undefined' && values.precoMax !== '' && parseInt(values.precoMin) > parseInt(values.precoMax)) {
+        errors.precoMin = 'Not valid precoMin';
+        errors.precoMax = 'Not valid precoMax';
+    }
+
+    return errors;
 };
 
 export default reduxForm({
     form: 'filter',
-    enableReinitialize: true
+    enableReinitialize: true,
+    validate
 })(FilterForm);
